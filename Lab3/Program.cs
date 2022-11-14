6f79373c28e41b9260cb229ef8af9a0eb7791a6b
beyenilmez <76536654+beyenilmez@users.noreply.github.com>
2022-11-14 20:47:24 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab3 {
+    internal class Program {
+        static void Main() {
+
+            //  Example 1
+            Console.Write("Put in number 1: ");
+            string? number1 = Console.ReadLine();
+            Console.Write("Put in number 2: ");
+            string? number2 = Console.ReadLine();
+
+            if (number1 == number2) {
+                Console.WriteLine("Yes");
+            }
+            else {
+                Console.WriteLine("No");
+            }
+
+            //  Example 2
+            Console.Write("Please put in an integer: ");
+            string number3 = Console.ReadLine();
+            bool number3State = Convert.ToInt32(number3) % 5 == 0;
+            Console.WriteLine(number3State);
+
+            //  Example 3
+            Console.Write("Please put in hour: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please put in minutes: ");
+            int minute = Convert.ToInt32(Console.ReadLine());
+
+            string timeFormat = " AM";
+
+            if (hour >= 13) {
+                hour = hour - 12;
+                timeFormat = " PM";
+            }
+
+            Console.WriteLine(hour + ":" + minute + timeFormat);
+
+            //  Example 4
+            Console.Write("Please put in your weight in kg: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please put in your height in m: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double index = weight / (height * height) * 100;
+
+            if (index < 18.5) {
+                Console.WriteLine("Thin");
+            }
+            else if (index < 25.0) {
+                Console.WriteLine("Normal");
+            }
+            else if (index < 30.0) {
+                Console.WriteLine("Fat");
+            }
+            else {
+                Console.WriteLine("Obese");
+            }
+
+
+            //  Example 5
+            Console.Write("Put in number 1: ");
+            double me = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Put in number 2: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            if ((me + n) % 2 == 0) {
+                Console.WriteLine(2 * me / (me * me - n * n));
+            }
+            else {
+                Console.WriteLine("0");
+            }
+
+
+            //  Example 6
+            Console.Write("Put in number 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Put in number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Put in number 3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2 && num1 == num3) {
+                Console.WriteLine("1 distinct value.");
+            }
+            else if ((num1 == num2 && num1 != num3) || (num1 == num3 && num1 != num2)) {
+                Console.WriteLine("2 distinct values.");
+            }
+            else {
+                Console.WriteLine("3 distinct values.");
+            }
+
+
+            //  Example 7
+            Console.Write("Please put in a month:");
+            string m = Console.ReadLine().ToLower();
+            Console.Write("Please put in a day:");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            if ((m == "march" && d >= 21) || m == "april" || m == "may" || (m == "june" && d <= 20)) {
+                Console.WriteLine("Spring");
+            }
+            else if ((m == "june" && d >= 21) || m == "july" || m == "august" || (m == "september" && d <= 22)) {
+                Console.WriteLine("Summer");
+            }
+            else if ((m == "september" && d >= 23) || m == "october" || m == "november" || (m == "december" && d <= 21)) {
+                Console.WriteLine("Fall");
+            }
+            else if ((m == "december" && d >= 21) || m == "january" || m == "february" || (m == "march" && d <= 20)) {
+                Console.WriteLine("Winter");
+            }
+            else {
+                Console.WriteLine("Error");
+            }
+
+            //  Example 8
+            double diameter, velocity, density, viscosity;
+
+            Console.Write("Please put in diameter: ");
+            diameter = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please put in velocity: ");
+            velocity = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please put in density: ");
+            density = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please put in viscosity: ");
+            viscosity = Convert.ToDouble(Console.ReadLine());
+
+            double rNumber = diameter * velocity * density / viscosity / 100;
+
+            if (rNumber < 2000) {
+                Console.WriteLine("laminar flow");
+                Console.WriteLine("\n {0} x {1} x {2} / {3} = {4}", density, velocity, diameter, viscosity, rNumber);
+                Console.WriteLine("\nReynolds number is less than 2000, so the flow of liquid is laminar.");
+            }
+            else if (rNumber < 4000) {
+                Console.WriteLine("transient flow");
+                Console.WriteLine("\n {0} x {1} x {2} / {3} = {4}", density, velocity, diameter, viscosity, rNumber);
+                Console.WriteLine("\nReynolds number is between 2000 and 4000, so the flow of liquid is transient.");
+            }
+            else {
+                Console.WriteLine("turbulent flow");
+                Console.WriteLine("\n {0} x {1} x {2} / {3} = {4}", density, velocity, diameter, viscosity, rNumber);
+                Console.WriteLine("\nReynolds number is more than 4000, so the flow of liquid is turbulent.");
+            }
+
+
+
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,185 @@
+﻿namespace Lab4 {
+    internal class Program {
+        static void Main(string[] args){
+
+            //  Question 1
+            Console.Write("Type in a number: ");
+            string? shapeSides = Console.ReadLine();
+            string shapeName;
+
+            switch (shapeSides) {
+                case "5":
+                    shapeName = "Pentagon";
+                    break;
+                case "6":
+                    shapeName = "Hexagon";
+                    break;
+                case "7":
+                    shapeName = "Heptagon";
+                    break;
+                case "8":
+                    shapeName = "Octagon";
+                    break;
+                default:
+                    shapeName = "Other";
+                    break;
+            }
+
+            Console.WriteLine(shapeName);
+
+            //  Question 2
+            for (int u = 3; u <= 87; u = u + 7) {
+                Console.Write(u + " ");
+            }
+
+            //  Question 3
+            int i = 3;
+            while (i <= 87) {
+                Console.Write(i + " ");
+                i = i + 7;
+            }
+
+            //  Question 4
+            Console.WriteLine("12 Midnight");
+            for (int k = 1; k < 12; k++) {
+                Console.WriteLine(k + "am");
+            }
+            Console.WriteLine("12 Noon");
+            for (int l = 1; l < 12; l++) {
+                Console.WriteLine(l + "pm");
+            }
+
+            //  Question 5
+            double sum = 0;
+
+            Console.Write("Type a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double j = 1;
+
+            while (j < n + 1) {
+                sum = sum + (1 / j);
+                j++;
+            }
+
+            Console.WriteLine(sum);
+
+            //  Question 6
+            Random random = new Random();
+            int count = 1;
+            int dice = 6; 
+
+            do {
+                dice = random.Next(1, 7);
+                Console.WriteLine(count + ". try        " + dice);
+                count++;
+            } while (dice != 6);
+
+            Console.WriteLine("It came up after {0} tries.", count);
+
+            //  Question 7
+            Console.Write("How many numbers will you type: ");
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            int minOddNum = int.MaxValue;
+            int currentNumber;
+
+            for (int p = 1; p < numberCount + 1; p++) {
+                Console.Write(p + ". Number: ");
+                currentNumber = Convert.ToInt32(Console.ReadLine());
+                if (currentNumber % 2 != 0 && currentNumber < minOddNum) {
+                    minOddNum = currentNumber;
+                }
+            }
+
+            if (minOddNum == 0) {
+                Console.WriteLine("There are no odd numbers");
+            }
+            else {
+                Console.WriteLine("The minimum odd number is " + minOddNum);
+            }
+
+            //  Question 8
+            Console.Write("Type in a number between 1 and 9: ");
+            string? number = Console.ReadLine();
+            string numberType = "Not a valid number";
+
+            switch (number) {
+                case "1":
+                case "4":
+                case "9":
+                    numberType = "Perfect Square";
+                    break;
+                case "2":
+                case "6":
+                case "8":
+                    numberType = "Even Number";
+                    break;
+                case "3":
+                case "5":
+                case "7":
+                    numberType = "Prime Number";
+                    break;
+            }
+
+            Console.WriteLine(numberType);
+
+            //  Question 9
+            string countType;
+            for (int b = 1; b < 11; b++) {
+                switch (b) {
+                    case 1:
+                        countType = "st";
+                        break;
+                    case 2:
+                        countType = "nd";
+                        break;
+                    case 3:
+                        countType = "rd";
+                        break;
+                    default:
+                        countType = "th";
+                        break;
+                }
+                Console.WriteLine("Height at the {0}{1} hour= {2}", b, countType, 0.12 * b * b * b * b + 12 * b * b * b - 380 * b * b + 4100 * b + 220);
+            }
+
+            //  Question 10
+            Random r = new Random();
+            int randomNumber = r.Next(1, 100);
+
+            int attempt = 1;
+
+            Console.Write("Enter a number: ");
+            int currNumber = Convert.ToInt32(Console.ReadLine());
+
+            do {
+                if (currNumber < randomNumber) {
+                    Console.Write("Please enter a higher number: ");
+                    currNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (currNumber > randomNumber) {
+                    Console.Write("Please enter a lower number: ");
+                    currNumber = Convert.ToInt32(Console.ReadLine());
+                }
+
+                attempt++;
+
+            } while (currNumber != randomNumber);
+
+            Console.WriteLine("You guessed the number after {0} attempts.", attempt);
+
+            //  Question 11
+            Console.Write("Type a number: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            double top = Math.PI;
+            double bottom = 2;
+
+            for (int a = 1; a < m + 1; a++) {
+                top = top * (2 * a - 1);
+                bottom = bottom * (2 * a);
+            }
+
+            Console.WriteLine(top / bottom);
+        }
+
+    }
+}
